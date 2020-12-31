533ba9f6a8525726a6cfba87922b27a8757662e2
WaleedAli9631 <waleedali9631@gmail.com>
2020-12-30 17:06:42 -0800

    recapcha test

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -7,6 +7,7 @@ import imageUrlBuilder from '@sanity/image-url'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { useForm } from "react-hook-form"
 import { navigate } from "gatsby"
+import ReCAPTCHA from "react-google-recaptcha"
 
 
 const ContactContent = styled.div`
@@ -84,23 +85,96 @@ const serializers = {
 
 const Contact = props =>  {
 
+       // Initiate forms
+       const { register, handleSubmit, errors, reset } = useForm()
+
+       // Transforms the form data from the React Hook Form output to a format Netlify can read
+       const encode = (data) => {
+         return Object.keys(data)
+           .map(
+             (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+           )
+           .join("&")
+       }
+     
+       // Handles the post process to Netlify so we can access their serverless functions
+       const handlePost = (formData, event) => {
+         fetch(`/`, {
+           method: "POST",
+           headers: { "Content-Type": "application/x-www-form-urlencoded" },
+           body: encode({ "form-name": "contact-form", ...formData }),
+         })
+           .then((response) => {
+             navigate("")
+             reset()
+            //  console.log(response)
+           })
+           .catch((error) => {
+            //  console.log(error)
+           })
+         event.preventDefault()
+       }
 
       return(
         <div className = {props.className}>
             <Layout>
                 <div className = "Content">
                 <PortableText blocks={props.body} serializers={serializers} />
-                <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
-                <p>
-                  <label>Email: <input type="text" name="name" /></label>
-                </p>
-                <p>
-                  <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <div data-netlify-recaptcha="true"></div>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
+                <form
+                onSubmit={handleSubmit(handlePost)}
+                name="contact-form"
+                method="POST"
+                data-netlify-recaptcha="true"
+                data-netlify="true"
+                netlify-honeypot="got-ya"
+              >
+                <input type="hidden" name="form-name" value="contact-form" />
+                <input
+                  type="hidden"
+                  name="formId"
+                  value="contact-form"
+                  ref={register()}
+                />
+                <label htmlFor="name">
+                  <p>Name</p>
+                  {errors.name && <span>Error message</span>}
+                  <input name="name" ref={register({ required: true })} />
+                </label>
+                <label htmlFor="email">
+                  <p>Email</p>
+                  {errors.email && <span>Please format email correctly</span>}
+                  <input
+                    name="email"
+                    ref={register({
+                      required: true,
+                      pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+                    })}
+                  />
+                </label>
+                <label htmlFor="message">
+                  <p>Message</p>
+                  <textarea rows="4" name="message" ref={register()} />
+                </label>
+                <label
+                  htmlFor="got-ya"
+                  style={{
+                    position: "absolute",
+                    overflow: "hidden",
+                    clip: "rect(0 0 0 0)",
+                    height: "1px",
+                    width: "1px",
+                    margin: "-1px",
+                    padding: "0",
+                    border: "0",
+                  }}
+                >
+                  Don’t fill this out if you're human:
+                  <input tabIndex="-1" name="got-ya" ref={register()} />
+                </label>
+                <ReCAPTCHA sitekey="6LdKCBsaAAAAAHYPAjzWbt054Nvff1kqHXAUsr6i" />
+                <div>
+                  <button type="submit">Submit</button>
+                </div>
               </form>
                 </div> 
                 
